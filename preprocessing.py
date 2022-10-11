afbea6cfe18149f9463c2ea7159c3bf7a31fc9ca
Dinh Minh <minhdinh.l138@gmail.com>
2022-10-12 01:50:05 +0700

    add 1st files

---
+++ preprocessing.py
@@ -0,0 +1,46 @@
+import unicodedata
+import regex as re
+import emoji
+
+# Xoa HTML code
+def remove_html(txt):
+    return re.sub(r'<[^>]*>', '', txt)
+
+# Xoa URL
+def remove_url(txt):
+    return re.sub(r'https\S+', '', txt, flags=re.MULTILINE)
+
+# Xoa emoji
+def remove_emoji(txt):
+    return emoji.replace_emoji(txt, replace='')
+
+def remove_emoji_unicode_type(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
+# Xoa khoang trang thua
+def remove_space(txt):
+    return re.sub(r'\s+', ' ', txt).strip()
+
+# Xoa hashtag
+def remove_hashtag(txt):
+    return re.sub("#[A-Za-z0-9_àáảãạâầấẩẫậăằắẳẵặèéẻẽẹêềếểễệđìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳýỷỹỵÀÁẢÃẠÂẦẤẨẪẬĂẰẮẲẴẶÈÉẺẼẸÊỀẾỂỄỆĐÌÍỈĨỊÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢÙÚỦŨỤƯỪỨỬỮỰỲÝỶỸỴÂĂĐÔƠƯ]+","", txt)
